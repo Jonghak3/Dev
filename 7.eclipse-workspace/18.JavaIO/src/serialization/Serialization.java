f2f70b19345fb6f6bfa0b135389d0b5b459fb901
Jonghak3 <ghdwhdgkr92@gamail.com>
2025-08-07 17:51:19 +0900

    8/7

---
+++ 7.eclipse-workspace/18.JavaIO/src/serialization/Serialization.java
@@ -0,0 +1,59 @@
+package serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+class Person implements Serializable {
+	//버전 관리를 위한 정보
+	private static final long serialVersionID = 15834564865L;
+	String name;
+	String job;
+	
+	public Person()  {}
+	
+	public Person(String name, String job) {
+		//super();
+		this.name = name;
+		this.job = job;
+	}
+
+	@Override
+	public String toString() {
+		return name +", " +job;
+	}
+}
+
+public class Serialization {
+
+	public static void main(String[] args) {
+		Person personLee = new Person("이순신", "대표이사");
+		Person personShin = new Person("신사임당", "상무이사");
+		
+		try(FileOutputStream fos = new FileOutputStream("serial.out");
+			ObjectOutputStream oos = new ObjectOutputStream(fos)){
+			oos.writeObject(personLee);		//personLee와 personShin의 값을 파일에 씀
+			oos.writeObject(personShin);	// (직렬화)
+			
+		} catch (IOException e) {
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+		try(FileInputStream fis = new FileInputStream("serial.out");
+			ObjectInputStream ois = new ObjectInputStream(fis)){
+			Person p1 = (Person) ois.readObject();	//personLee와 personShin의 값을
+			Person p2 = (Person) ois.readObject();	//파일에서 읽어 들임 (역직렬화)
+			
+			System.out.println(p1);
+			System.out.println(p2);
+		} catch (IOException | ClassNotFoundException e) {
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+			}
+	}
+}
